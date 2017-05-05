57b6976fbe64e2426dae8c1c26c3bae856cc5989
artiventura <artiventura.club@gmail.com>
2017-05-05 07:44:30 -0500

    Create ErickCallSkype.js

---
+++ ErickCallSkype.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="https://secure.skypeassets.com/i/scom/js/skype-uri.js"></script>
+<div id="SkypeButton_Call_erick.pve_1">
+ <script type="text/javascript">
+ Skype.ui({
+ "name": "dropdown",
+ "element": "SkypeButton_Call_erick.pve_1",
+ "participants": ["erick.pve"],
+ "imageSize": 24
+ });
+ </script>
+</div>
